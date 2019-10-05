4cabc2f222c24bf897d68079475ab799719b6b93
stephen-l <stephen.j.ludlow@gmail.com>
2019-10-05 18:00:11 +1000

    convert to py3

--- xor-decrypt-pattern-find.py
+++ xor-decrypt-pattern-find.py
@@ -25,31 +25,31 @@ with open(sys.argv[1], 'r') as file: # read encrypted file
 n = 40 # length of key
 substr = Counter(s[i: i+n] for i in range(len(s) - n))
 key, count = substr.most_common(1)[0]
-print ' [+] 40 char common string: %r' % (key)
-print ' [+] Finding substring'
+print(' [+] 40 char common string: %r' % (key))
+print(' [+] Finding substring')
 
 key = repeats(key) # remove repetition
 n = len(key)
 
 if n != 32:
-    print ' [+] New key'
+    print(' [+] New key')
 
-print ' [+] Rotating key'
-print ' [+] Looking for MZ'
+print(' [+] Rotating key')
+print(' [+] Looking for MZ')
 
 for i in key: # rotate the key until "MZ\x90" is found
     key = rotate(key,1)
     if "M" == chr(ord(s[0]) ^ ord(key[0])) and "Z" == chr(ord(s[1]) ^ ord(key[1])) and "\x90" == chr(ord(s[2]) ^ ord(key[2])):
-            print ' [+] MZ header found'
-            print ' [+] Key = %r' % key
+            print(' [+] MZ header found')
+            print(' [+] Key = %r' % key)
             break
 
 if not len(sys.argv) == 3: # quick cli args
-    print "Usage: %s encryptedfile outputfile" % sys.argv[0]
+    print("Usage: %s encryptedfile outputfile" % sys.argv[0])
 else:
     with open(sys.argv[1], 'r') as myfile:
         data=myfile.read()
-    print ' [+] Writing executable'
+    print(' [+] Writing executable')
     fo = open(sys.argv[2], "wb")
     fo.write(xor(data,key)) # write the new file to disk
     fo.close()
